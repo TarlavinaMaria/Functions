c6a04362aae6610890c834a033fae16db36fccf3
Мария Тарлавина <tarlavina50@mail.ru>
2023-03-02 15:02:13 +0700

    Encoding

--- Functions_DZ/Functions_DZ.cpp
+++ Functions_DZ/Functions_DZ.cpp
@@ -1,10 +1,50 @@
 ﻿#include<iostream>
 using namespace std;
+double Factorial(int n, double f);
+double Power(double a, double n, double res);
 
+//#define FACTORIAL
+//#define POWER
 
-
-void main() 
+void main()
 {
-	setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "");
+#ifdef FACTORIAL
+    int n;
+    double f = 1;
+    cout << "Введите число для вычисления факториала "; cin >> n;
+    cout << "Факториал для числа " << n << "! = " << Factorial(n, f) << endl;
+#endif // FACTORIAL
 
+#ifdef POWER
+    double n, a;
+    double res = 1;
+    cout << "Введите число и степень "; cin >> a >> n;
+    cout << "Число " << a << " в степени " << n << " = " << Power(a, n, res) << endl;
+#endif // POWER
 }
+
+    double Factorial(int n, double f)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            f *= i;
+        }
+        return f; 
+    }
+    double Power(double a, double n, double res)
+    {
+        if (n < 0)
+        {
+            a = 1 / a;
+            n = -n;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            res *= a;
+        }
+        return res;
+    }
+
+
+
