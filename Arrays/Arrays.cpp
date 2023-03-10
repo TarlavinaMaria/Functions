d1176d8d1b13b1d314ee26b6b7487a733aeca93b
Мария Тарлавина <tarlavina50@mail.ru>
2023-03-10 21:23:22 +0700

    Encoding

--- Arrays/Arrays.cpp
+++ Arrays/Arrays.cpp
@@ -1,39 +1,70 @@
 ﻿#include<iostream>
 using namespace std;
 
-
+void FillRand(int arr[], const int n);
+void Print(const int arr[], const int n);
+int Sum(const int arr[], const int n);
+double Avg(const int arr[], const int n);
+int minValueIn(const int arr[], const int n);
+int maxValueIn(const int arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 5;
-	int arr[n];
-	int sum = 0;
-	/*arr[2] = 123;
-	cout << arr[2] << endl;*/
-	cout << "Введите элементы массива: ";
+	int arr[n] = { 3,5,8 };
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Mаксимальное значение массива: " << maxValueIn(arr,n) << endl;
+	cout << "Mинимальное значение массива: " << minValueIn(arr,n) << endl;
+	
+}
+void FillRand(int arr[], const int n)
+{
+	// возвращает псевдо случайное число
 	for (int i = 0; i < n; i++)
 	{
-		cin >> arr[i];
+		arr[i] = rand() % 100;
 	}
+}
+void Print(const int arr[], const int n)
+{
+	// вывод массива на экран:
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
-	cout << endl;
+}
+int Sum(const int arr[], const int n)
+{
+	int sum = 0;
 	for (int i = 0; i < n; i++)
 	{
 		sum += arr[i];
 	}
-	cout << "Сумма: " << sum << endl;
-	cout << "Среднее арифметическое: " << (double)sum / n << endl;
-	int max = arr[0];
+	return sum;
+}
+double Avg(const int arr[], const int n)
+{
+	return (double)Sum(arr, n)/n;
+}
+int minValueIn(const int arr[], const int n)
+{
 	int	min = arr[0];
 	for (int i = 0; i < n; i++)
 	{
-		if (max < arr[i]) max = arr[i];
 		if (min > arr[i]) min = arr[i];
 	}
-	cout << "Max: " << max << endl;
-	cout << "Min: " << min << endl;
+	return min;
+}
+int maxValueIn(const int arr[], const int n)
+{
+	int	max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (max < arr[i]) max = arr[i];
+	}
+	return max;
 }
